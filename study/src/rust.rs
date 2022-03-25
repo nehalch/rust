1096c7cf3b5bd879cff63bff8539a04e2d3645d8
Andrew <rtpdfv@yahoo.com>
2022-03-25 23:13:06 +0100

    <T> type

--- study/src/rust.rs
+++ study/src/rust.rs
@@ -263,3 +263,25 @@ fn file_found(i:bool) -> Result<i32,bool> {
 }
 
 if file_found(true).is_ok(){}
+
+
+// <T> type
+// structs 
+// func
+// trait
+// enum
+// vec
+// collections
+
+fn main(){
+   println!("- Passing a string literal"); 
+   concatenate(" Rust ", " Programming "); 
+   println!("- Passing an integer"); 
+   concatenate(10 as i32, 1 as i32);
+   
+}
+use std::fmt::Display;
+fn concatenate<T:Display>(t:T, s:T){
+   let result = format!("{}{}", t , s);
+   println!("{}", result);
+}
